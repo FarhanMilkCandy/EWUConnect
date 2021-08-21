6bc1dbc0488e9c81adb64a56e903205e5f106185
FarhanMilkCandy <59729868+FarhanMilkCandy@users.noreply.github.com>
2021-08-21 14:04:40 +0600

    Bugfix
    login and profile model is fixed now

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
 from django.forms import ModelForm
 
 from .models import *
